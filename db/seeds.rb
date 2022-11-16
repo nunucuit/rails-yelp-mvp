8d95fd566e8b54779f0c994162b5cfdb9cec86e4
nunucuit <rises-426@hotmail.fr>
2022-11-16 14:24:58 +0100

    trop cool

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create(name: "Georgio", address: 'Nice', phone_number: "04 65 21 42", category: "chinese")
+Restaurant.create(name: "Paquito", address: 'Gotham city', phone_number: "04 68 21 59 65", category: "italian")
+Restaurant.create(name: "Boulin", address: 'Cannes', phone_number: "04 66 21 59 65", category: "japanese")
+Restaurant.create(name: "KFC", address: 'Kitchencity', phone_number: "04 69 21 59 65", category: "french")
+Restaurant.create(name: "Macdo", address: 'Groscity', phone_number: "04 61 21 59 65", category: "belgian")
